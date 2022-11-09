e5cc29bc3c65ab00f0b37d182f386c21abdd67f0
jtrachsel <julestrachsel@gmail.com>
2022-11-09 10:16:09 -0600

    copy soupx output figures

--- scripts/COPY_RESULTS.sh
+++ scripts/COPY_RESULTS.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p /project/fsep_004/jtrachsel/john_lipolis_milk_blood/outputs/figures/
+
+cp SoupX/*jpeg /project/fsep_004/jtrachsel/john_lipolis_milk_blood/outputs/figures/
 cp -r scripts/ /project/fsep_004/jtrachsel/john_lipolis_milk_blood/
 cp -r outputs/ /project/fsep_004/jtrachsel/john_lipolis_milk_blood/
 #cp -r renv/ /project/fsep_004/jtrachsel/john_lipolis_milk_blood/
