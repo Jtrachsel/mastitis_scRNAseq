7af8e0c8b4f3e6127fbbe5ac9452f8de0bb387cc
Julian Trachsel <julestrachsel@gmail.com>
2022-12-16 22:19:31 +0000

    cleanup

--- scripts/create_seurat_reference_mapping_objects.R
+++ scripts/create_seurat_reference_mapping_objects.R
@@ -6,7 +6,7 @@ library(SeuratDisk)
 library(Matrix)
 library(biomaRt)
 
-
+# function to parse the gencode.v30.annotation.gtf
 gtf_parse <- function(path){
   #browser()
   gtf <- read_delim(path,
@@ -90,11 +90,14 @@ map_IDs_to_ensembl <-
   }
 
 # public AWS S3 bucket (https://registry.opendata.aws/tabula-sapiens/)
-# tabula used ENCODE GRCh38
-# nyquist used GRCh37
-# wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_30/gencode.v30.annotation.gtf.gz
+# tabula used GENCODE GRCh38
 
 #### for reference mapping ####
+
+
+# This gtf annotation file was used by the tabula sapiens atlas
+# it contains information to convert between the gene IDs used and emsembl_IDs
+# wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_30/gencode.v30.annotation.gtf.gz
 
 ENCODE30_GTF <- gtf_parse('gencode.v30.annotation.gtf')
 
@@ -116,7 +119,7 @@ BOVINIZER <- human_homologs %>% transmute(BOVINE_ID=ensembl_gene_id, ensembl_gen
 
 # Tabula sapiens whole blood data
 
-# some growing pains in the communtiy regarding anndata to seurat formats
+# some problems converting anndata to seurat formats
 # https://github.com/mojaveazure/seurat-disk/issues/109
 
 tabula_blood_anndata <- anndata::read_h5ad('reference_mapping_data/tabula_10X_blood.h5ad')
@@ -125,24 +128,19 @@ tabula_blood_counts <- Matrix::t(tabula_blood_anndata$X)
 
 # remove 0 count genes
 tabula_blood_counts <- tabula_blood_counts[rowSums(tabula_blood_counts) != 0,]
-## RENAME HERE
-# listMarts()
-# listEnsembl()
-# useEnsembl(GRCh = 37, 'genes', dataset=)
 
-# mart=useEnsembl('regulation')
-# listDatasets(mart)
 
-# JOIN TO DATA FROM GTF
+# strips the version decimal off the ensembl gene id
 ENCODE30_mapping <-
   ENCODE30_GTF %>%
   dplyr::transmute(gene_name, ensembl_gene_id=sub('([A-Z0-9]+)\\.[0-9]+','\\1',gene_id))
 
+# join the gtf based mappings to the IDs present in the data
 tabula_blood_mapping <- 
   tibble(gene_name=rownames(tabula_blood_counts)) %>% 
   left_join(ENCODE30_mapping)
 
-
+# join in the data linking one to one homologs with our data
 bovinized_tabula_blood <- 
   tabula_blood_mapping %>% 
   left_join(BOVINIZER) %>%
@@ -150,11 +148,11 @@ bovinized_tabula_blood <-
   left_join(our_genes)
 
 
-
+# make a little "dictionary" that converts 'tabula sapiens IDs' to 'our IDs'
 tabula_swapper <- bovinized_tabula_blood$our_id
 names(tabula_swapper) <- bovinized_tabula_blood$gene_name
 
-# subset to only genes with bovine homologs
+# subset the data to only genes with bovine homologs
 tabula_blood_counts <- tabula_blood_counts[rownames(tabula_blood_counts) %in% bovinized_tabula_blood$gene_name,]
 
 
@@ -165,25 +163,26 @@ rownames(tabula_blood_counts) <- tabula_swapper[rownames(tabula_blood_counts)]
 names(rownames(tabula_blood_counts)) <- NULL
 rownames(tabula_blood_counts)
 
-# bovinized tabula whole blood
-# no need for biomaRt because we used the gtf from encode
-# tabula_blood_counts
-
-
+# make a seurat object from the renamed counts and the metadata from the anndata object
 tabula_blood <- CreateSeuratObject(counts = tabula_blood_counts, meta.data = tabula_blood_anndata$obs)
 
+# double check that we dont have any genes with all zeros
 non_zero_genes <- rowSums(tabula_blood) > 0
 tabula_blood_filt <- tabula_blood[non_zero_genes,]
 
+# write out object
 SaveH5Seurat(tabula_blood_filt, 'reference_mapping_data/tabula_sapiens_blood', overwrite=TRUE)
 
 
-### MIT human breast milk study 
 
-# uses weird genenames
-# some are gene symbols and some are embl ids?
-# info on embl accession in this article
-# https://academic.oup.com/nar/article/28/1/19/2384388
+###
+
+### MIT human breast milk study 
+# nyquist used GRCh37
+# past version of human genome
+
+###
+
 Nyquist_counts <- 
   data.table::fread('reference_mapping_data/MIT_Milk_Study_Raw_counts.txt') %>% 
   Matrix::as.matrix(rownames='GENE')
@@ -196,11 +195,7 @@ Nyquist_meta <- Nyquist_meta[-1,] %>% column_to_rownames(var = 'NAME')
 
 
 
-
-
-
 ## PREVIOUS VERSION OF HUMAN GENOME BECAUSE THIS IS WHAT MIT MILK DATA USES
-# IS THIS NECESSARY?
 
 # The nyquist MIT milk data uses an old version of the human genome
 # clone based assembly.
@@ -215,20 +210,12 @@ grch37 <- useMart(biomart="ENSEMBL_MART_ENSEMBL", host="grch37.ensembl.org",
 # 
 # 
 ATTRIBUTES <- listAttributes(grch37)
-# ALL_IDs <- ATTRIBUTES %>% 
-#   filter(page =='feature_page') %>% 
-#   dplyr::select(name, description) %>% 
-#   filter(grepl('ID', description)) %>% 
-#   # filter(grepl('ensembl'))
-#   pull(name)
+
 
 ACCESSIONS <- 
   ATTRIBUTES %>% 
   filter(page =='feature_page') %>% 
   dplyr::select(name, description) %>%
-  # filter(!(name %in% NOT_THESE)) %>%
-  # filter(grepl('name|symbol', name)) %>% 
-  # filter(grepl('ID', description)) %>% 
   filter(grepl('accession', name)) %>% 
   pull(name)
 
@@ -236,9 +223,6 @@ NAMES <-
   ATTRIBUTES %>% 
   filter(page =='feature_page') %>% 
   dplyr::select(name, description) %>%
-  # filter(!(name %in% NOT_THESE)) %>%
-  # filter(grepl('name|symbol', name)) %>% 
-  # filter(grepl('ID', description)) %>% 
   filter(grepl('name', name)) %>% 
   pull(name)
 
@@ -246,15 +230,12 @@ CLONES <-
   ATTRIBUTES %>% 
   filter(page =='feature_page') %>% 
   dplyr::select(name, description) %>%
-  # filter(!(name %in% NOT_THESE)) %>%
-  # filter(grepl('name|symbol', name)) %>% 
-  # filter(grepl('ID', description)) %>% 
   filter(grepl('clone_based', name)) %>% 
   pull(name)
 
 
 
-
+# list of attributes to try checking these IDS against
 ATTRIBUTES <- 
   c('hgnc_symbol',
     'entrezgene_accession',
@@ -272,48 +253,21 @@ ATTRIBUTES <-
 
 
 
-
+# run the mapping function
 nyquist_mapping_results <-
   map_IDs_to_ensembl(ID_vector = rownames(Nyquist_counts), 
                      attributes_vector = ATTRIBUTES, mart = grch37)
 
-
+# calculate % IDs mapped
 NUM_UNMAPPED <- nyquist_mapping_results %>% filter(is.na(mapping_type)) %>% nrow()
 NUM_MAPPED <- nrow(nyquist_mapping_results)
 PERCENT_MAPPED <- (NUM_MAPPED / (NUM_MAPPED + NUM_UNMAPPED)) * 100
 
-# 
-# grch37_mapping <- 
-#   getBM(values=rownames(Nyquist_counts),
-#         attributes= c("ensembl_gene_id",
-#                       'external_gene_name',
-#                       'clone_based_ensembl_gene',
-#                       'clone_based_vega_gene',
-#                       'description'),
-#         mart= grch37) %>% as_tibble()
-# 
-# 
-# nyquist_mapping <- tibble(nyquist_ID=rownames(Nyquist_counts), 
-#                           external_gene_name=ifelse(nyquist_ID %in% grch37_mapping$external_gene_name,nyquist_ID, NA), 
-#                           clone_based_ensembl_gene=ifelse(nyquist_ID %in% grch37_mapping$clone_based_ensembl_gene,nyquist_ID, NA), 
-#                           clone_based_vega_gene=ifelse(nyquist_ID %in% grch37_mapping$clone_based_vega_gene,nyquist_ID, NA)) 
-# 
-# ensembl_external_map <- 
-#   grch37_mapping %>% 
-#   dplyr::select(ensembl_gene_id, external_gene_name) %>% 
-#   unique() 
-# 
-# 
-# nyquist_mapping <- nyquist_mapping %>% left_join(ensembl_external_map) 
+# 99%+
 
-
-# all but 183 gene_IDs from the nyquist study are mapped to ensembl's external_gene_name vector
-# I couldn't find the remaining 183 genes in clone based mappings...
-# oh well, cant be perfect... moving on.
 nyquist_bovine_mapping <- 
   nyquist_mapping_results %>%
   transmute(nyquist_ID=ID, ensembl_gene_id) %>% 
-  # dplyr::select(nyquist_ID, ensembl_gene_id) %>% 
   left_join(BOVINIZER) %>% 
   filter(!is.na(BOVINE_ID)) %>% 
   dplyr::select(nyquist_ID, BOVINE_ID) %>%
@@ -330,6 +284,7 @@ multi_mapped <-
 nyquist_bovine_mapping <- 
   nyquist_bovine_mapping %>% 
   filter(!(our_id %in% multi_mapped))
+
 #when provided an id from the nyquiust object the nyquist swapper will return the corresponding ID 
 # from our study
 nyquist_swapper <- nyquist_bovine_mapping$our_id
